96402e9dcc35a31fc91f48a2172d9bb1e254cda7
troyefun <fanghongyu@pku.edu.cn>
2023-04-09 23:32:35 +0800

    fix bug; add voxel_feature and cam_pts visualization

---
+++ test_utils/main.py
@@ -0,0 +1,46 @@
+import ipdb
+import matplotlib.cm as cm
+import numpy as np
+from sklearn.decomposition import PCA
+
+import test_utils
+
+
+def show_cam_pts_and_voxel_feature():
+    cam_pts = np.load('tmp_data/cam_pts.npy')
+    voxel_feature = np.load('tmp_data/mock_voxel_feature.npy')
+    pts_feature = np.load('tmp_data/mock_pts_feature.npy')  # (n_pts, C)
+
+    cam_pts = cam_pts.reshape(-1, 3)  # (n_pts, 3) -- (x, y, z)
+    C, D, H, W = voxel_feature.shape
+    voxel_feature = np.transpose(voxel_feature, (2, 3, 1, 0))  # (H, W, D, C) -- (x, y, z, feature)
+    voxel_feature = voxel_feature.reshape(-1, C)  # (n_voxel, C)
+
+    # pca = PCA(3)
+    # pca.fit(voxel_feature)
+    # voxel_color = pca.fit_transform(voxel_feature)
+    # pts_color = pca.fit_transform(pts_feature)
+    # min_value, max_value = voxel_color.min(), voxel_color.max()
+    # voxel_color = (voxel_color - min_value) / (max_value - min_value)
+    # pts_color = (pts_color - min_value) / (max_value - min_value)
+    cmap = cm.get_cmap('Spectral')
+    voxel_color = cmap(voxel_feature[:, 0] * 3)[:, :3]
+    pts_color = cmap(pts_feature[:, 0] * 3)[:, :3]
+
+    scene_range = ((-25.6, 25.6), (-51.2, 51.2), (-5.0, 3.0))
+    axis_x = np.linspace(scene_range[0][0], scene_range[0][1], H + 1)[:-1] + 0.5 * (scene_range[0][1] - scene_range[0][0]) / H
+    axis_y = np.linspace(scene_range[1][0], scene_range[1][1], W + 1)[:-1] + 0.5 * (scene_range[1][1] - scene_range[1][0]) / W
+    axis_z = np.linspace(scene_range[2][0], scene_range[2][1], D + 1)[:-1] + 0.5 * (scene_range[2][1] - scene_range[2][0]) / D
+    grid_x, grid_y, grid_z = np.meshgrid(axis_x, axis_y, axis_z, indexing='ij')
+    voxel_grid = np.stack([grid_x, grid_y, grid_z], axis=-1)
+    voxel_grid = voxel_grid.reshape(-1, 3)
+    voxel_pcd = test_utils.get_pcd(voxel_grid, voxel_color)
+    cam_pcd = test_utils.get_pcd(cam_pts, pts_color)
+    ipdb.set_trace()
+    test_utils.show_pcds([voxel_pcd, cam_pcd])
+    test_utils.show_pcd(cam_pcd)
+    test_utils.show_pcd(voxel_pcd)
+
+
+if __name__ == '__main__':
+    show_cam_pts_and_voxel_feature()
--- test_utils/utils.py
+++ test_utils/utils.py
@@ -1,5 +1,10 @@
+import os
+
 import matplotlib.pyplot as plt
+import numpy as np
+import open3d as o3d
 from PIL import Image
+import torch
 
 
 def show_img(img):
@@ -11,3 +16,57 @@ def show_img(img):
         img = Image.open(img)
     plt.imshow(img)
     plt.show()
+
+
+def dump_tensor(save_name, tensor):
+    """Dump tensor to numpy array.
+    Args:
+        tensor (torch.Tensor): Tensor to dump.
+    Returns:
+        np.ndarray: Numpy array of the tensor.
+    """
+    os.makedirs('tmp_data', exist_ok=True)
+    array = tensor.detach().cpu().numpy()
+    np.save(os.path.join('tmp_data', save_name + '.npy'), array)
+
+
+def get_pcd(points, colors=None):
+    """Get open3d point cloud from points and colors.
+    Args:
+        points (np.ndarray): Points of point cloud.
+        colors (np.ndarray, optional): Colors of point cloud.
+            Defaults to None.
+    Returns:
+        open3d.geometry.PointCloud: Open3d point cloud.
+    """
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(points)
+    if colors is not None:
+        pcd.colors = o3d.utility.Vector3dVector(colors)
+    return pcd
+
+
+def show_pcd(pcd):
+    """Show open3d point cloud.
+    Args:
+        pcd (open3d.geometry.PointCloud): Open3d point cloud.
+    """
+    o3d.visualization.draw_geometries([pcd])
+
+
+def show_pcds(pcds):
+    """Show multiple open3d point clouds.
+    Args:
+        pcds (list[open3d.geometry.PointCloud]): Open3d point clouds.
+    """
+    o3d.visualization.draw_geometries(pcds)
+
+
+def save_mock_voxel_feature(voxel_feature, cam_pts, scene_range, sample_feats_3d_fn):
+    C, D, H, W = voxel_feature.shape
+    mock_voxel_feature = torch.arange(3 * D * H * W).type_as(voxel_feature).reshape(3, D, H, W)
+    mock_voxel_feature /= (3 * D * H * W)
+    mock_pts_feature = sample_feats_3d_fn(mock_voxel_feature, cam_pts, scene_range)
+    dump_tensor('mock_voxel_feature', mock_voxel_feature)
+    dump_tensor('mock_pts_feature', mock_pts_feature)
+    dump_tensor('cam_pts', cam_pts)
