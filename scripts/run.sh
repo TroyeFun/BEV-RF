f8f12774927e92fbfe202e5d70aa42813e2d9786
troyefun <fanghongyu@pku.edu.cn>
2023-05-03 13:19:13 +0800

    run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
-DATE=`date +%y%m%d%H%M`
+DATE=`date +%y%m%d`
 # torchpack dist-run -np 1 python tools/train.py configs/nuscenes/seg/fusion-bev256d2-lss.yaml --model.encoders.camera.backbone.init_cfg.checkpoint pretrained/swint-nuimages-pretrained.pth
 # torchpack dist-run -np 1 python tools/train.py configs/bevnerf_nuscenes/bevnerf/fusion-bev256d2-lss.yaml --model.encoders.camera.backbone.init_cfg.checkpoint pretrained/swint-nuimages-pretrained.pth
 # torchpack dist-run -np 2 python tools/train.py configs/bevnerf_nuscenes/bevnerf/fusion-bev256d2-lss.yaml --model.encoders.camera.backbone.init_cfg.checkpoint pretrained/swint-nuimages-pretrained.pth --load_from pretrained/bevfusion-seg-half_x_range.pth --no-validate
